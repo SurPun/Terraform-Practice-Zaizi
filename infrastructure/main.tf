a4edfe7883c11774de5f80717e18e267231b8c39
Suraj <S_pun@outlook.com>
2023-04-18 15:05:13 +0100

    Created Public Vpc & subnet

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,7 +4,26 @@ provider "aws" {
   profile                 = "aws-cred"
 }
 
-// Create ec2 Instance-1
+// Create A Public VPC & Subnet 
+resource "aws_vpc" "public-vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "Public-VPC"
+  }
+}
+
+resource "aws_subnet" "Public-subnet" {
+  vpc_id     = "aws_vpc.public-vpc"
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Public-Subnet"
+  }
+}
+
+
+// Create EC2 Instance-1
 resource "aws_instance" "Ubuntu_Instance" {
   ami           = "ami-0cd8ad123effa531a"
   instance_type = "t2.micro"

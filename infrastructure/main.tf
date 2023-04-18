9f53813f425b17d8e055c437908a9c2ed474427a
Suraj <spun@zaizi.com>
2023-04-18 13:25:22 +0100

    aws linked & ec2 instance created

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,1 +1,15 @@
-# AWS Resource - EC2 Instance
+provider "aws" {
+  region                  = "eu-west-2"
+  shared_credentials_file = "../.aws/credentials"
+  profile                 = "aws-cred"
+}
+
+// Create ec2 Instance-1
+resource "aws_instance" "Ubuntu_Instance" {
+  ami           = "ami-0cd8ad123effa531a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Instance"
+  }
+}
